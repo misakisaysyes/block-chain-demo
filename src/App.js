d9623907215ab9a4a6fd4b1f9850bf4ea5c6b819
tanjia <misakisaysyes@gmail.com>
2020-11-17 18:37:40 +0800

    fix前端转账功能

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect } from 'react'
 import { UserOutlined, PayCircleOutlined, BankOutlined, SwapOutlined } from '@ant-design/icons'
 import { Url, ContractAddr, Abi} from './ config'
 import './App.css'
@@ -6,46 +6,58 @@ import Web3 from 'web3'
 
 const web3 = new Web3(new Web3.providers.HttpProvider(Url))
 const contractHandler = new web3.eth.Contract(Abi, ContractAddr)
-let Ctx = React.createContext(null)
 
 const App = () => {
-  const [accounts, setAccounts] = useState(null)
-  const [cur, setCur] = useState(0)
   const [minter, setMinter] = useState('')
-  const [op, setOp] = useState('mint')
+  const [selectedAccount, setSelectedAccount] = useState('')
+  const [op, setOp] = useState({
+    opType: 'mint',
+    field: 'to',
+  })
 
-  useEffect(()=>{
-    web3.eth.getAccounts().then(res => {
-      setAccounts(res)
-    }).catch(err => {
-      console.log(`fail to get accounts:${err}`)
-    })
-
-    /* egcall */
+  useEffect(() => {
+    // 查询minter
     contractHandler.methods.minter().call({}, (err, res) => {
       err && console.log(`failed to get minter:${err}`)
-      res && setMinter(res)
+      res && setMinter(res) 
     })
   }, [])
 
-  return (
+   return (
     <div className='app'> 
-      <Ctx.Provider value={{ accounts, cur, op, minter }}>
-        <Accounts setCur={ cur => setCur(cur) }/>
-        <Operations setOp={ op => setOp(op) }/>
-      </Ctx.Provider> 
+      <Accounts 
+        minter={minter} 
+        selectAccount={item => setSelectedAccount(item)} 
+      />
+      <Operations  
+        op={op} 
+        minter={minter}
+        setOp={item => setOp(c => ({ ...c, ...item}))}
+        selectedAccount={selectedAccount}
+      />
     </div>
-  )
+   )
 }
 
 const Accounts = props => {
-  const { setCur } = props
-  const { accounts, cur, minter, op } = useContext(Ctx)
+  const { minter, selectAccount } = props
+  const [accountList, setAccountList] = useState([])
   const [balances, setBalances] = useState([])
-  
+  const [cur, setCur] = useState(0)
+
   useEffect(() => {
+    // 取所有账户
+    // 用以太坊接口查询
+    web3.eth.getAccounts().then(res => {
+      setAccountList(res)
+    }).catch(err => {
+      console.log(`fail to get accountList:${err}`)
+    })
+
+    // 查询每个账户的余额
+    // 用合约接口查询
     let promises = []
-    accounts && accounts.forEach(el => {
+    accountList && accountList.forEach(el => {
       const p = new Promise((resolve, reject) => 
         contractHandler.methods.balances(el).call({}, (err, res) => {
           resolve(res)
@@ -57,16 +69,16 @@ const Accounts = props => {
     Promise.allSettled(promises).then(res => {
       setBalances(res.filter(el => el.status === 'fulfilled').map(el => el.value ? el.value : 0))
     })
-  }, [accounts, minter, op])
+  },[accountList])
 
   return (
     <div className='account-wrapper'>
       {
-        accounts && accounts.map((el, idx) => (
+        accountList && accountList.map((el, idx) => (
           <div 
             key={el}  
-            className={`account-row${el === minter ? ' minter': ''}${idx === cur ? ' to' : '' }`}
-            onClick={() => setCur(idx)} 
+            className={`account-row${el === minter ? ' minter': ''}${ cur === idx ? ' to' : '' }`}
+            onClick={() => { selectAccount(el); setCur(idx) }}
           >
             <div> <UserOutlined /> {`  ${el}`} </div>
             <div> <PayCircleOutlined /> {`  ${balances[idx]}`} </div>
@@ -78,139 +90,83 @@ const Accounts = props => {
 }
 
 const Operations = props => {
-  const { setOp } = props
-  const { op } = useContext(Ctx)
+  const { op, setOp, selectedAccount, minter } = props
+  const { opType, field } = op
+  const [accounts, setAccounts] = useState({
+    from: '',
+    to: '',
+  })
+
+  useEffect(() => {
+    opType === 'mint' && accounts.from !== minter && setAccounts(c => ({...c, from: minter}))
+    const obj = field === 'from' ? { from: selectedAccount } : { to: selectedAccount }
+    setAccounts(c => ({...c, ...obj}))
+  }, [selectedAccount])
+ 
+  const eventProxy = e => {
+    e.target && e.target.id && setOp({ field: e.target.id })
+    if(e.target.id === 'submit') {
+      switch(opType) {
+        case 'mint':
+          contractHandler.methods.mint(
+            accounts.to,
+            10
+          ).send({ from: minter })
+          .on('receipt', receipt => {
+            console.log(`发币完成`, receipt)
+            alert('发币成功')
+          })
+          break
+        case 'send':
+            contractHandler.methods.send(
+              accounts.to,
+              10
+            ).send({ from: accounts.from })
+            .then(receipt => {
+              console.log('转账完成', receipt)
+              alert('转账成功')
+            })
+          break
+        default:
+      }
+    }
+  }
+
+  const toggleOp = op => {
+    const resetAccounts = op === 'mint' ? { from: minter, to: '' } : { from: '', to: '' }
+    const resetField = op === 'mint' ? { opType:op, field: 'to' } : { opType: op, field: 'from'}
+    setOp(resetField)
+    setAccounts(resetAccounts)
+  }
+
   return (
     <div className='op-wrapper'>
-      { /mint/.test(op) && <OpBlock  
-        onClick={_op => setOp(_op)}
-      />}
-      { op === 'transfer' && <OpBlock  
-        onClick={() => setOp('mint')}
-      />}
+      <div className='op-block' onClick={ e => eventProxy(e)}>
+        <div className='op-header'>
+          <div > {op.opType === 'mint' ? 
+            (<span onClick={() => toggleOp('send')}><BankOutlined />  发币</span>) : 
+            (<span onClick={() => toggleOp('mint')}><SwapOutlined />  转帐</span>)}
+          </div> 
+        </div>
+        <div>
+          <div id='from' className={`btn ${field === 'from' && 'btn-active'}`} >from</div>
+          {accounts.from}
+        </div>
+        <div>
+          <div id='to' className={`btn ${field === 'to' && 'btn-active'}`} >to</div>
+          {accounts.to}
+        </div>
+        <div>
+          <div className='btn' >amount</div>
+          {10}
+        </div>
+        <div>
+          <div id='submit' className={`btn ${field === 'submit' && 'btn-active'}`} >{ opType }</div>
+        </div>
+      </div>
     </div>
   )
 }
 
-const OpBlock = props => {
-  const { accounts, cur, op, minter } = useContext(Ctx)
-
-  const mint = () => {
-    /* egsend */
-    contractHandler.methods.mint(
-      accounts[cur],
-      10
-    ).send({ from: minter })
-    .on('transactionHash', hash => {
-      console.log(`查看transactionHash`, hash)
-    })
-    .on('receipt', receipt => {
-      console.log(`查看receipt$`, receipt)
-      props.onClick(`${op}-${cur + 1}`)
-      alert('发币成功')
-    })
-  }
-
-  return (
-    <div className='op-block' >
-      <div className='op-header'>
-        { /mint/.test(op) ? <div><BankOutlined />{' 发币'}</div> : <div><SwapOutlined />{' 转帐'}</div> }
-      </div>
-      <div>
-        <div className='btn'>from</div>
-        { minter }
-      </div>
-      <div>
-        <div className='btn'>to</div>
-        { accounts && accounts[cur] }
-      </div>
-      <div>
-        <div className='btn'>amount</div>
-        {'10'}
-      </div>
-      <div>
-        <div className='btn' onClick={() => {mint()}}>mint</div>
-      </div>
-  </div>
-  )
-}
-
-// const originRef = () => {
-//   const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
-  
-//   web3.eth.getAccounts().then(res => {
-//     console.log('获取账户', res)
-//   })
-
-//   console.log('查看web3.eth', web3.eth)
-
-//   const contractHandler = new web3.eth.Contract(abi, '0x74fcacced25c52520df07cd280cee3c116a62013')
-//   console.log('查看合约句柄', contractHandler)
-
-//   contractHandler.methods.minter().call({
-//     from: '0x116173eb2001B80e65Ce452983286fd9fCce2916'
-//   }, (err, res) => {
-//     err && console.log('查看minter失败', err)
-//     res && console.log('minter：', res)
-//   })
-
-//   contractHandler.methods.mint(
-//     '0xb0036A897EabF7BeB551f93A06dC7D91Dcf5C97B',
-//     100
-//   ).call({
-//     from: '0x116173eb2001B80e65Ce452983286fd9fCce2916',
-//     gasPrice: '1',
-//     gas: 9999999999,
-//   }, (err, res) => {
-//     err && console.log('发布失败', err)
-//     res && console.log('发币成功', res)
-//   })
-
-//   contractHandler.methods.mint(
-//     '0xb0036A897EabF7BeB551f93A06dC7D91Dcf5C97B',
-//     1
-//   ).send({ from: '0x116173eb2001B80e65Ce452983286fd9fCce2916'})
-//   .on('transactionHash', hash => {
-//     console.log('查看transactionHash', hash)
-//   })
-//   // .on('receipt', receipt => {
-//   //   console.log('查看receipt', receipt)
-//   // })
-//   // .on('confirmation', (confirmationNumber, receipt) => {
-//   //   console.log('查看confirmation', confirmationNumber, receipt)
-//   // })
-//   // .on('error', (error, receipt) => {
-//   //   console.log('查看error', error, receipt)
-//   // })
-
-//   contractHandler.methods.balances('0xb0036A897EabF7BeB551f93A06dC7D91Dcf5C97B').call({
-//     from: '0x116173eb2001B80e65Ce452983286fd9fCce2916',
-//   }, (err, res) => {
-//     err && console.log('看余额失败', err)
-//     res && console.log('看余额成功', res)
-//   })
-
-//   contractHandler.methods.send(
-//     '0x116173eb2001B80e65Ce452983286fd9fCce2916',
-//     10
-//   ).send({from: '0xb0036A897EabF7BeB551f93A06dC7D91Dcf5C97B'}).then(
-//     receipt => {
-//       console.log('转账完成', receipt)
-//     }
-//   )
-
-//   contractHandler.methods.balances('0x116173eb2001B80e65Ce452983286fd9fCce2916').call({
-//     from: '0xb0036A897EabF7BeB551f93A06dC7D91Dcf5C97B',
-//   }, (err, res) => {
-//     err && console.log('查看矿工余额失败', err)
-//     res && console.log('查看矿工余额成功', res)
-//   })
-
-//   return (
-//     <div> 
-//       this is test web3
-//     </div>
-//   );
-// }
-
 export default App
+
